a805b00200fd62e8153e3093a5d43ea5f86f3a06
naeunk1m <08enmik@gmail.com>
2023-07-08 15:52:02 +0900

    20230708

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,59 @@
 $(function () {
-  var headerInfo = new Swiper(".header-bnr", {
-    effect: "fade",
 
-    autoplay: {
-      delay: 5000,
+  var BntSwiper = new Swiper(".bnr-slide", {
+    effect: "fade",
+      loop: true,
+      autoplay: {
+      delay: 4000,
       disableOnInteraction: false,
-    },
+      }
   });
+
+  $(window).trigger('scroll');
+  $('.moreBtn').click(function () {
+    $('header-btm').toggleClass('active');
+    $('.btm-all').stop().slideToggle();
+  });
+
+  $('btn-more').click(function () {
+    if($('menu-list').hasClass('active')){
+      $('menu-list.all').addClass('show')
+      $('menu-list.active').removeClass('active')
+    }else{
+      $('menu-list.active').addClass('active')
+      $('menu-list.all').removeClass('show')
+    }
+  })
+
+  let lastScroll = 0;
+
+  $(window).scroll(function(){
+    curr = $(this).scrollTop();
+    if(curr > 0){
+        $('.header').addClass('fixed')
+    }else{
+        $('.header').removeClass('fixed')
+    }
+
+    if(curr == 0){
+
+      $('.fix-btn').removeClass('show')
+
+    }else{
+
+      if (curr > lastScroll) {
+        $('.fix-btn').removeClass('show')
+      } else {
+        $('.fix-btn').addClass('show')
+      }
+    }
+    lastScroll = curr;
+
+  })
+
+  $('#topBtn').click(function(e){
+    e.preventDefault();
+    window.scrollTo({top:0,behavior:'smooth'})
+  })
+
 });
